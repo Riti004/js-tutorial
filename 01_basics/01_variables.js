30ab2c201f1607dc133b71d24d80cbcdb53fbf9c
Ritika Adak <167062204+Riti004@users.noreply.github.com>
2025-11-03 11:57:06 +0000

    learnt about variables and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14456;
+let accountEmail="ritika@gmail.com";
+var accountPassword="pass123"
+accountAdress="kolkata";//possible
+let accountState;
+// accountId=2;//not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountState]);
+/*
+prefer not to use var because of block scope and functional scope 
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";//treats all js code as newer version 
+//alert(3+3)//we are using node js not browser
+
+
+//number->
+//string->
+//boolean->
+//null->standalone value ->console.log(typeof null);o/p-> object 
+//undefined->not assigned yet ->console.log(typeof undefined);o/p-> undefine 
+//symbol->uniqe
+//object->
+
+console.log(typeof "ritika");
+console.log(typeof null);
+console.log(typeof undefined);
