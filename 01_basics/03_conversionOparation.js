94cf3352bdde40f781a35a19cba5642333607c95
Ritika Adak <167062204+Riti004@users.noreply.github.com>
2025-11-03 12:17:04 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOparation.js
@@ -0,0 +1,26 @@
+//let score="22abc";//not a pure number 
+
+//console.log(typeof(score));//o/p->string 
+//converts string into number 
+//let valueNumber=Number(score);
+//console.log(typeof(valueNumber));//o/p-> number 
+//console.log(valueNumber)//o/p->not a number 
+
+//"33"=>number;
+//"33abc"=>NaN;
+//true=>1;false=>0;
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn =Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1=>true;
+//0=>false;
+//"ritika"=>true;
+//""=>false;
+
+let someNumber=33;
+let someNumberType=String(someNumber);
+console.log(someNumberType);
+console.log(typeof someNumberType);
